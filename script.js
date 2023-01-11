beeeb6292bd4f8977b0a235081d9d410ebc9a090
Florent Marchal <fmarchal@seifel.fr>
2023-01-11 19:05:37 +0100

    timeline ok

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 gsap.registerPlugin(DrawSVGPlugin, MotionPathPlugin);
 
-gsap.set("#car", {xPercent: -50, yPercent: -50, transformOrigin: "center center"})
+gsap.set("#car", { xPercent: -50, yPercent: -50, transformOrigin: "center center" })
 
-var tl4 = gsap.timeline({ delay: 18.6, defaults: { duration: 3, ease: "none", immediateRender: true } });
+var tl4 = gsap.timeline({ delay: 21.6, defaults: { duration: 3, ease: "none", immediateRender: true } });
 tl4.from("#path-p-r", { drawSVG: 0 })
     .to("#car", {
         motionPath: {
@@ -14,7 +14,7 @@ tl4.from("#path-p-r", { drawSVG: 0 })
 
 
 
-var tl3 = gsap.timeline({ delay: 11, defaults: { duration: 3, ease: "none", immediateRender: true } });
+var tl3 = gsap.timeline({ delay: 13, defaults: { duration: 3, ease: "none", immediateRender: true } });
 tl3.from("#path-a-a", { drawSVG: 0 })
     .to("#car", {
         motionPath: {
@@ -23,11 +23,11 @@ tl3.from("#path-a-a", { drawSVG: 0 })
             autoRotate: true
         },
     }, 0);
-tl3.tweenTo(0.8, { delay: 11 });
-tl3.tweenFromTo(0.8, 1.1, { delay: 13.8 });
-tl3.tweenFromTo(1.1, 3, { delay: 16.9 });
+tl3.tweenTo(0.8, { delay: 13 });
+tl3.tweenFromTo(0.8, 1.1, { delay: 16.8 });
+tl3.tweenFromTo(1.1, 3, { delay: 19.9 });
 
-var tl2 = gsap.timeline({ delay: 6, defaults: { duration: 3, ease: "none", immediateRender: true } });
+var tl2 = gsap.timeline({ delay: 8, defaults: { duration: 3, ease: "none", immediateRender: true } });
 tl2.from("#path-p-a", { drawSVG: 0 })
     .to("#car", {
         motionPath: {
@@ -37,7 +37,18 @@ tl2.from("#path-p-a", { drawSVG: 0 })
         }
     }, 0);
 
-var tl = gsap.timeline({ delay: 1, defaults: { duration: 3, ease: "none", immediateRender: true } });
+//var tlinfo2 = gsap.timeline({ delay: 8, defaults: { duration: 3, ease: "none", immediateRender: true } });
+//tlinfo2.from("#path-p-a", { drawSVG: 0 })
+//    .to("#infosAmsterdam", {
+//        motionPath: {
+//            path: "#path-p-a",
+//            align: "#path-p-a",
+//        }
+//    }, 0);
+//tlinfo2.tweenTo(3, { delay: 11 });
+
+
+var tl = gsap.timeline({ delay: 3, defaults: { duration: 3, ease: "none", immediateRender: true } });
 tl.from("#path", { drawSVG: 0 })
     .to("#car", {
         motionPath: {
@@ -47,8 +58,9 @@ tl.from("#path", { drawSVG: 0 })
         }
     }, 0);
 
+
 tl.add(TweenLite.to(".RNSPAR", 1, { css: { className: 'departure-board RNSPAR' } }), 0.11);
 tl2.add(TweenLite.to(".PARAMS", 1, { css: { className: 'departure-board PARAMS mt-1' } }), 0.1);
 tl3.add(TweenLite.to(".AMSANR", 1, { css: { className: 'departure-board AMSANR mt-1' } }), 0.1);
-tl3.add(TweenLite.to(".ANRBRU", 1, { css: { className: 'departure-board ANRBRU mt-1' } }), 0.5);
-tl3.add(TweenLite.to(".BRURNS", 1, { css: { className: 'departure-board BRURNS mt-1' } }), 1);
+tl3.add(TweenLite.to(".ANRBRU", 1, { css: { className: 'departure-board ANRBRU mt-1' } }), 1);
+tl3.add(TweenLite.to(".BRURNS", 1, { css: { className: 'departure-board BRURNS mt-1' } }), 2);
