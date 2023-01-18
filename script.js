938c465315aae464186c80cc0450cabda9bd7a2d
Florent Marchal <fmarchal@seifel.fr>
2023-01-18 13:39:03 +0100

    layout ok

--- script.js
+++ script.js
@@ -1,51 +1,10 @@
 gsap.registerPlugin(DrawSVGPlugin, MotionPathPlugin);
 
 gsap.set("#car", { xPercent: -50, yPercent: -50, transformOrigin: "center center" })
-
-//var tl4 = gsap.timeline({ delay: 21.6, defaults: { duration: 3, ease: "none", immediateRender: true } });
-//tl4.from("#path-p-r", { drawSVG: 0 })
-//    .to("#car", {
-//        motionPath: {
-//            path: "#path-p-r",
-//            align: "#path-p-r",
-//            autoRotate: true
-//        }
-//    }, 0);
-
-
-
-//var tl3 = gsap.timeline({ delay: 13, defaults: { duration: 3, ease: "none", immediateRender: true } });
-//tl3.from("#path-a-a", { drawSVG: 0 })
-//    .to("#car", {
-//        motionPath: {
-//            path: "#path-a-a",
-//            align: "#path-a-a",
-//            autoRotate: true
-//        },
-//    }, 0);
-//tl3.tweenTo(0.8, { delay: 13 });
-//tl3.tweenFromTo(0.8, 1.1, { delay: 16.8 });
-//tl3.tweenFromTo(1.1, 3, { delay: 19.9 });
-
-//var tl2 = gsap.timeline({ delay: 8, defaults: { duration: 3, ease: "none", immediateRender: true } });
-//tl2.from("#path-p-a", { drawSVG: 0 })
-//    .to("#car", {
-//        motionPath: {
-//            path: "#path-p-a",
-//            align: "#path-p-a",
-//            autoRotate: true
-//        }
-//    }, 0);
-
-//var tlinfo2 = gsap.timeline({ delay: 8, defaults: { duration: 3, ease: "none", immediateRender: true } });
-//tlinfo2.from("#path-p-a", { drawSVG: 0 })
-//    .to("#infosAmsterdam", {
-//        motionPath: {
-//            path: "#path-p-a",
-//            align: "#path-p-a",
-//        }
-//    }, 0);
-//tlinfo2.tweenTo(3, { delay: 11 });
+gsap.set("#paris-dot", { xPercent: -50, yPercent: -50, transformOrigin: "center center" })
+gsap.set("#anvers-dot", { xPercent: -50, yPercent: -50, transformOrigin: "center center" })
+gsap.set("#amsterdam-dot", { xPercent: -50, yPercent: -50, transformOrigin: "center center" })
+gsap.set("#brussels-dot", { xPercent: -50, yPercent: -80, transformOrigin: "center center" })
 
 
 var tl = gsap.timeline({ delay: 0.1, defaults: { duration: 3, ease: "none", immediateRender: true } });
@@ -59,11 +18,28 @@ tween = gsap.to("#car", {
             autoRotate: true
     },
     duration: 3,
-    ease: "none"
+    ease: "none",
+    onComplete: function () {
+        $("#parisdiv").css('display', 'flex');
+    }
     }, 0);
 tl.add(tween, "0");
 
+
+tl.addLabel("paris");
+//tween = gsap.to("#parisdiv", {
+//    motionPath: {
+//        path: "#paris-dot",
+//        align: "#paris-dot",
+//    },
+//    opacity: 1,
+//    translate: '1em',
+//    duration: 0,
+//    ease: "none"
+//}, 0);
+//tl.add(tween, "paris");
 tl.addLabel("paris", "+=2");
+
 var tween = gsap.from("#path-p-b", { drawSVG: 0, duration: 4, ease: "none" });
 tl.add(tween, "paris");
 tween = gsap.to("#car", {
@@ -101,9 +77,26 @@ tween = gsap.to("#car", {
         autoRotate: true
     },
     duration: 2,
-    ease: "none"
+    ease: "none",
+    onComplete: function () {
+        $("#amsterdamdiv").css('display', 'flex');
+    }
 }, 0);
 tl.add(tween, "anvers");
+
+
+tl.addLabel("amsterdam");
+//tween = gsap.to("#amsterdamdiv", {
+//    motionPath: {
+//        path: "#amsterdam-dot",
+//        align: "#amsterdam-dot",
+//    },
+//    opacity: 1,
+//    translate: '-' + $("#amsterdamdiv").width() * 1.5 + 'px',
+//    duration: 0,
+//    ease: "none"
+//}, 0);
+//tl.add(tween, "amsterdam");
 
 tl.addLabel("amsterdam", "+=2");
 var tween = gsap.from("#path-a-a-r", { drawSVG: 0, duration: 3, ease: "none" });
@@ -115,9 +108,28 @@ tween = gsap.to("#car", {
         autoRotate: true
     },
     duration: 3,
-    ease: "none"
+    ease: "none",
+    onComplete: function () {
+        $("#anversdiv").css('display', 'flex');
+    }
 }, 0);
 tl.add(tween, "amsterdam");
+
+
+tl.addLabel("anvers2");
+//tween = gsap.to("#anversdiv", {
+//    motionPath: {
+//        path: "#anvers-dot",
+//        align: "#anvers-dot",
+//    },
+//    opacity: 1,
+//    translate: '-' + $("#anversdiv").width() * 1.5 + 'px',
+//    duration: 0,
+//    ease: "none"
+//}, 0);
+//tl.add(tween, "anvers2");
+
+
 
 tl.addLabel("anvers2", "+=2");
 var tween = gsap.from("#path-b-a-r", { drawSVG: 0, duration: 3, ease: "none" });
@@ -129,11 +141,31 @@ tween = gsap.to("#car", {
         autoRotate: true
     },
     duration: 3,
-    ease: "none"
+    ease: "none",
+    onComplete: function () {
+        $("#brusselsdiv").css('display', 'flex');
+    }
 }, 0);
 tl.add(tween, "anvers2");
 
+
 tl.addLabel("brussels2");
+//tween = gsap.to("#brusselsdiv", {
+//    motionPath: {
+//        path: "#brussels-dot",
+//        align: "#brussels-dot",
+//    },
+//    opacity: 1,
+//    translate: '1em',
+//    duration: 0,
+//    ease: "none"
+//}, 0);
+//tl.add(tween, "brussels2");
+
+
+
+
+tl.addLabel("brussels2", "+=2");
 var tween = gsap.from("#path-p-b-r", { drawSVG: 0, duration: 3, ease: "none" });
 tl.add(tween, "brussels2");
 tween = gsap.to("#car", {
@@ -146,6 +178,7 @@ tween = gsap.to("#car", {
     ease: "none"
 }, 0);
 tl.add(tween, "brussels2");
+
 
 tl.addLabel("paris2");
 var tween = gsap.from("#path-p-r-r", { drawSVG: 0, duration: 3, ease: "none" });
